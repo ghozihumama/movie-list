2f03effa91d26c45b955ee834cbcf57cf83cbe4a
maulanaghozi <maulana.ghozi@qbit.co.id>
2022-05-30 16:46:01 +0700

    delete home module css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,33 @@
 import { FC } from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #000;
+  color: #fff;
+`;
 
 const Navbar: FC = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <div>
+        <Link href={"/"}>
+          <a>NEXT</a>
+        </Link>
+      </div>
+      <div>
+        <Link href={"/"}>
+          <a>Home</a>
+        </Link>
+        <Link href={"/about"}>
+          <a>About</a>
+        </Link>
+        <Link href={"/contact"}>
+          <a>Contact</a>
+        </Link>
+      </div>
+    </Nav>
   );
 };
 
